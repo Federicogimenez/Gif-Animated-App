7fb2e59e748b5242cf5657a00f4029a9fd2c3424
Federico <gimanezf@gmail.com>
2022-02-05 18:57:59 -0300

    rename

--- src/GifAnimatedApp.js
+++ src/GifAnimatedApp.js
@@ -13,7 +13,7 @@ export const GifAnimatedApp = () => {
   
     return(
       <>
-        <h1>Gif Expert App</h1>
+        <h1>Gif Animated App</h1>
         <AddCategory setCategories={setCategories}/>
         <hr></hr>
             {            
